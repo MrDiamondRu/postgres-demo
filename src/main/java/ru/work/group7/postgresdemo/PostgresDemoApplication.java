b36819d29f9b2ba92980b69766b3f83cc5b7334a
Ruslan <r.nikonov-pro@yandex.ru>
2023-07-27 16:12:51 +0300

    Initial commit

---
+++ src/main/java/ru/work/group7/postgresdemo/PostgresDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.work.group7.postgresdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/work/group7/postgresdemo/PostgresDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.work.group7.postgresdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
